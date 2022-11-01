43c2797c36f5256a5598d528ab96a148b628cb38
Nikolay <nk@jquad.de>
2022-11-01 13:31:28 +0100

    intro-to-vue3 step 4. Conditional Rendering

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,6 +6,8 @@ export default defineComponent({
     return {
       title: "This title is from Vue Component data",
       color: "color: red;",
+      inStock: true, // change this attribute to see effect
+      inventory: 11, // change this attribute to see effect
     };
   },
 });
@@ -16,5 +18,15 @@ export default defineComponent({
     <h1 v-bind:style="color">{{ title }}</h1>
     <!-- short way to bind -->
     <h1 :style="color">{{ title }}</h1>
+
+    <p v-if="inStock">v-if Cool TShirts In Stock</p>
+    <p v-else>v-else Cool TShirts Out of Stock</p>
+    <p v-show="inStock">v-show Cool TShirts Out of Stock</p>
+
+    <p v-if="inventory > 10">v-if More than 10 Cool TShirts In of Stock</p>
+    <p v-else-if="inventory <= 10 && inventory > 0">
+      v-else-if Less than 10 Cool TShirts In of Stock
+    </p>
+    <p v-else>v-else Invetory 0, Out of Stock</p>
   </div>
 </template>
