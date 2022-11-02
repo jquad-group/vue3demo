9640a786ec2db18cd30f6aa83105f7460957e1ef
Nikolay <nk@jquad.de>
2022-11-02 19:54:37 +0100

    intro-to-vue3 step 2. Creating the Vue App

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
+<script lang="ts" setup>
+import { reactive } from "vue";
+
+// step 2. Creating the Vue App
+const stateTitle = reactive({ title: "This title is from Vue Component data" });
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ stateTitle.title }}</h1>
   </div>
 </template>
