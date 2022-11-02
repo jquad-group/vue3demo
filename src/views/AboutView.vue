13a43a4f807a2980003f539e257bf876550778b3
Nikolay <nk@jquad.de>
2022-11-02 19:58:52 +0100

    intro-to-vue3 step 3. Attribute binding

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,10 +3,16 @@ import { reactive } from "vue";
 
 // step 2. Creating the Vue App
 const stateTitle = reactive({ title: "This title is from Vue Component data" });
+// step 3 . Attribute binding
+const stateTitleColor = reactive({ color: "color: red;" });
 </script>
 
 <template>
   <div class="about">
     <h1>{{ stateTitle.title }}</h1>
+
+    <h1 v-bind:style="stateTitleColor.color">{{ stateTitle.title }}</h1>
+    <!-- short way to bind -->
+    <h1 :style="stateTitleColor.color">{{ stateTitle.title }}</h1>
   </div>
 </template>
